b5cf5d0251ac8b7ac7bd7b40337a18a106ddc3fe
rhishikeshj <rhishikeshj@gmail.com>
2024-02-08 22:05:29 +0100

    make progress fn generic over any iterable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::{thread::sleep, time::Duration};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
@@ -6,16 +7,28 @@ fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
 
-fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
+fn progress<Iter>(iter: Iter, f: fn(Iter::Item) -> ())
+where
+    Iter: Iterator,
+{
     let mut i = 1;
-    for n in v.iter() {
+    for n in iter {
         println!("{}{}", CLEAR, "*".repeat(i));
         i += 1;
         f(n)
     }
 }
+
 fn main() {
     let v = vec![1, 2, 3];
-    let k = vec!["a", "b", "c", "d"];
-    progress(v, expensive_calculation);
+
+    progress(v.iter(), expensive_calculation);
+
+    let mut h = HashSet::new();
+    h.insert(0);
+    h.insert(2);
+    h.insert(4);
+    h.insert(6);
+
+    progress(h.iter(), expensive_calculation);
 }
